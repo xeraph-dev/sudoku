c6abb29fa670ff06424e678850a102d0fcd3c8ab
Xeraph <adrianrlc99@gmail.com>
2022-08-28 22:33:12 -0400

    add production mode optimization

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
 import App from './App.svelte'
 
+if (import.meta.env.MODE === 'production') {
+  document.addEventListener(
+    'contextmenu',
+    e => {
+      e.preventDefault()
+      return false
+    },
+    { capture: true },
+  )
+
+  document.addEventListener(
+    'selectstart',
+    e => {
+      e.preventDefault()
+      return false
+    },
+    { capture: true },
+  )
+}
+
 const app = new App({
   target: document.getElementById('app'),
 })
